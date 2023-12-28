19e59e4258f40cb97408005f470929d5a991c334
asalinas10 <a_salinas1996@yahoo.com>
2023-12-27 16:17:28 -0800

    PyBank updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,27 +3,31 @@
 import os
 import csv
 
-# set csv file to variable
+# impor csv file to variable
 
 pybankcsv = os.path.join("Resources", "budget_data.csv")
 
-# 
-
-total_profits = 0
+# create lists for months and toal profits/losses
 
 Months = []
 
 total_pl = []
 
+# open csv file
+
 with open(pybankcsv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     
-    csv_header = next(csvfile)
-    #print(f"Header: {csv_header}")
+    # Set header line aside
 
-    
+    csv_header = next(csvfile)
+
+    # set profits/losses counter to 0
 
     gross_pl = 0
+
+
+    # append Months, total_pl lists and gross_pl counter
 
     for row in csvreader:
         Months.append(row[0])
@@ -34,59 +38,71 @@ with open(pybankcsv) as csvfile:
 
         gross_pl += prof_loss
 
-    #print(Months)
+    # find total months
+    total_months = int(len(Months))
 
-    average_pl = round(((total_pl[85] - total_pl[0]) / 86), 2)
+    print(total_months) #86
+
+    # find average profit/loss by dividing total_pl over total_months 
+
+    average_pl = round(((total_pl[85] - total_pl[0]) / total_months), 2)
+
+    
+    # next section is to find which months had the single greatest increase and decrease in profits from whole data set
+    
+    # set month_increase to first index of total_pl for starting point
+
+    month_increase = total_pl[0]
+
+    # set loop that checks if month_increase is less than the current index of total_pl
+
+    for i in range(len(total_pl)):
+        if month_increase < total_pl[i]:
+
+            # if month_increase is less than total_pl[i] then replace month_increase with total_pl[i] as it is now
+            month_increase = total_pl[i]
+
+            # if month_increase is equivalent to total_pl[i] then set high_month to Months[i]
+
+            if month_increase == total_pl[i]:
+                high_month = Months[i]
+                    
+
+    # set month_decrease to first index of total_pl for starting point
+
+    month_decrease = total_pl[0]
+
+    # set loop that checks if month_decrease is greater than the current index of total_pl
+
+    for i in range(len(total_pl)):
+        if month_decrease > total_pl[i]:
+
+            # if month_decrease is greater than total_pl[i] then replace month_decrease with total_pl[i] as it is now
+            month_decrease = total_pl[i]
+
+            # if month_decrease is equivalent to total_pl[i] then set low_month to Months[i]
+
+            if month_decrease == total_pl[i]:
+                low_month = Months[i]
 
     
 
-    total_months = int(len(Months)) 
-
-    ###average_profits = total_profits / total_months
-
-
-    max_increase = total_pl[0]
-    for i in range(len(total_pl)):
-        if max_increase < total_pl[i]:
-            max_increase = total_pl[i]
-            if max_increase == total_pl[i]:
-                high_month = Months[i]
-                    
-    #print(high_month)
-
-    max_decrease = total_pl[0]
-    for i in range(len(total_pl)):
-        if max_decrease > total_pl[i]:
-            max_decrease = total_pl[i]
-            if max_decrease == total_pl[i]:
-                low_month = Months[i]
-
-    #print(low_month)
-
-    print("Financial Analysis")
-
-    print("---------------------------------------------")
-
-    print(f"Total Months: {total_months}")
-
-    print(f"Total: ${gross_pl}")
-
-    print(f"Average Change: ${average_pl}")
-
-    print(f"Greatest Increase in Profits: {high_month} (${max_increase})")
-
-    print(f"Greatest Decrease in Profits: {low_month} (${max_decrease})")
-
-    #print(total_pl)
-
-    ###print(average_profits)
+# set up export path for analysis file
 
 output_path = os.path.join("Analysis", "bank_analysis.txt")
 
+# using the export path create a text file with neccesary financial information
 
 with open(output_path, 'w') as txtfile:
 
-    lines = ['Financial Analysis\n', '-------------------------------------------------\n', f"Total Months: {total_months}\n", f"Total: ${gross_pl}\n", f"Average Change: ${average_pl}\n", f"Greatest Increase in Profits: {high_month} (${max_increase})\n", f"Greatest Decrease in Profits: {low_month} (${max_decrease})\n"]
+    lines = ['Financial Analysis\n', 
+             '-------------------------------------------------\n', 
+             f"Total Months: {total_months}\n", 
+             f"Total: ${gross_pl}\n", 
+             f"Average Change: ${average_pl}\n", 
+             f"Greatest Increase in Profits: {high_month} (${month_increase})\n", 
+             f"Greatest Decrease in Profits: {low_month} (${month_decrease})\n", 
+             '-------------------------------------------------\n']
 
     txtfile.writelines(lines)
 
